7922068145a7eb7f32b76e7b33f1b0fc03d14639
Matteo Morganti <matteo99morganti@gmail.com>
2022-04-06 17:14:06 +0200

    Create index.blade and create.blade admin

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
 Auth::routes();
 
@@ -24,6 +21,11 @@ Route::middleware('auth')
     ->name('admin.')
     ->prefix('admin')
     ->group(function () {
-        Route::get('/home', 'HomeController@index')->name('home');
+        Route::get('/', 'HomeController@index')->name('home');
+
+        Route::resource('posts', 'PostController');
     });
 
+Route::get("{any?}", function() {
+    return view('guests.home');
+})->where("any", ".*");
